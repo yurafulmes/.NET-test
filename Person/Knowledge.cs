c4b2863aab7d21ba3cd7e891a1fe4eb10449e2e5
Yurii Fulmes <jurafulmes@gmail.com>
2019-03-27 00:58:35 +0200

    Added logic of Knowledge and Student classes

--- Person/Knowledge.cs
+++ Person/Knowledge.cs
@@ -2,9 +2,25 @@ namespace InternshipTest
 {
     public class Knowledge
     {
+        public int Level { get; }
         public Knowledge(int level)
         {
-            //TODO: Implementation is needed
+            Level = level;
+        }
+        //for overloading operator >,<
+        public int CompareTo(Knowledge other)
+        {
+            if (other == null)
+                return 1;
+            return Level.CompareTo(other.Level);
+        }
+        public static bool operator >(Knowledge k1, Knowledge k2)
+        {
+            return k1.CompareTo(k2) > 0 ? true : false;
+        }
+        public static bool operator <(Knowledge k1, Knowledge k2)
+        {
+            return k1.CompareTo(k2) < 0 ? true : false;
         }
     }
 }
--- Person/Student.cs
+++ Person/Student.cs
@@ -2,14 +2,28 @@ namespace InternshipTest.Person
 {
     public class Student
     {
-        public Student(string name)
+        public string Name { get; }
+        private Knowledge _knowledge;
+        public Knowledge Knowledge
         {
-            //TODO: Implementation is needed
+            get
+            {
+                return _knowledge ?? new Knowledge(0);//if null,then - first initialization 
+            }
+            private set
+            {
+                _knowledge = value;
+            }
+        }//On my opinion,without method "setKnowledge" — (get;set;) would be better
+        public Student(string name, Knowledge knowledge = null /*for flexibility can be an interface*/)
+        {
+            Name = name;
+            _knowledge = knowledge;
         }
 
         public void SetKnowledge(Knowledge knowledge)
         {
-            //TODO: Implementation is needed
+            Knowledge = knowledge;
         }
     }
 }
